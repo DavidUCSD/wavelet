fa8b47ad8a6c33ddf889d69d5455da46f6ec337d
DavidUCSD <floresdavid217@gmail.com>
2023-01-26 12:09:32 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arraylist;
+
+
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
